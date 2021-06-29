49ab561118bc8003fd14764034e6314ff7198f43
Maksim Kostromin <daggerok@gmail.com>
2021-06-29 20:44:22 +0300

    Implement simple and easy by path solution | bumping poms (C) whatthecommit.com :)

--- src/.vuepress/components/ArticleIndex.vue
+++ src/.vuepress/components/ArticleIndex.vue
@@ -27,24 +27,14 @@ export default {
   computed: {
     articles() {
       const pathStartsWithArticles = page => page.path.startsWith('/articles/') && !page.frontmatter.article_index;
-      // const byPageDateDesc = (page1, page2) => page1.frontmatter.date && page2.frontmatter.date
-      //     ? new Date(page1.frontmatter.date) - new Date(page2.frontmatter.date)
-      //     : new Date(page1.lastUpdated) - new Date(page2.lastUpdated);
-      // const byPageDateDesc = (page1, page2) => page1.frontmatter.date - page2.frontmatter.date;
       const byPagePath = (page1, page2) => {
         if (page1.path > page2.path) return 1;
         if (page1.path < page2.path) return -1;
         return 0;
       }
-      const byPageDate = (page1, page2) => {
-        if (page1.frontmatter.date > page2.frontmatter.date) return 1;
-        if (page1.frontmatter.date < page2.frontmatter.date) return -1;
-        return 0;
-      }
-      console.log('this.$page', this.$page);
       return this.$site.pages
                  .filter(pathStartsWithArticles)
-                 .sort(byPageDate)
+                 .sort(byPagePath)
                  .reverse();
     }
   },
--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,83 +1,10 @@
-const { spawnSync } = require('child_process');
-const moment = require('moment');
-const path = require('path');
-const fs = require('fs');
-
 const base = process.env.BASE || '/';
 
-function getGitLastUpdated(filePath) {
-  const globbyFilter = spawnSync(
-    'git',
-    ['log', '-1', '--format=%at', path.basename(filePath)],
-    { cwd: path.dirname(filePath) }
-  );
-  const readable = globbyFilter.stdout;
-  const string = readable.toString('utf-8');
-  const number = parseInt(string);
-  const timestamp = number * 1000
-  const date = new Date(timestamp);
-  console.log('date', date);
-  return date;
-}
-
-function getFsLastUpdated(filePath) {
-  const stats = fs.statSync(filePath);
-  const mtime = stats.mtime;
-  console.log('mtime', mtime);
-  return mtime;
-}
-
-function getLastUpdated(filePath) {
-  if (!filePath || !fs.existsSync(filePath))
-    /*throw `file ${filePath} doesn't exists`*/ return;
-  try {
-    return getGitLastUpdated(filePath);
-  }
-  catch (e) {
-    return getFsLastUpdated(filePath);
-  }
-}
-
-function dateToString(lastUpdated) {
-  const res = moment(lastUpdated).locale('ru-UA')
-                                 .format();
-  console.log('lastUpdated', lastUpdated, 'res', res);
-  return res;
-}
-
 module.exports = {
-  extendPageData($page) {
-    const {
-      _filePath,           // file's absolute path
-      _computed,           // access the client global computed mixins at build time, e.g _computed.$localePath.
-      _content,            // file's raw content string
-      _strippedContent,    // file's content string without frontmatter
-      key,                 // page's unique hash key
-      frontmatter,         // page's frontmatter object
-      regularPath,         // current page's default link (follow the file hierarchy)
-      path,                // current page's real link (use regularPath when permalink does not exist)
-    } = $page;
-    if ($page.frontmatter.date) return;
-    const lastUpdated = getLastUpdated(_filePath);
-    $page.frontmatter.date = dateToString(lastUpdated);
-  },
   base,
   title: 'Maksim Kostromin',
   description: 'I\'m doing software engineering, love and peace (;',
   plugins: [
-    // ['@vuepress/plugin-last-updated', {
-    //   dateOptions: {
-    //     hour12: false,
-    //     locales: 'ru_RU',
-    //   },
-    //   transformer: (timestamp, lang) => {
-    //     // moment.locale(lang);
-    //     const res = moment(timestamp)//.locale('ru-RU')
-    //                                  .format();
-    //     console.log('timestamp', timestamp, 'lang', lang, 'res', res);
-    //     return res;
-    //   },
-    // }],
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
     ['@vuepress/plugin-blog', {
@@ -97,7 +24,7 @@ module.exports = {
     }],
   ],
   themeConfig: {
-    lastUpdated: 'Last Updated',
+    lastUpdated: false, // 'Last Updated',
     author: 'Maksim Kostromin',
     nav: [
       { text: 'Home', link: '/' },
